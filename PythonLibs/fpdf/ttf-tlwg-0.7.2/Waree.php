902e41497f63e0f6855b813ad648bf3bea094564
Marklawrence12 <76943222+Marklawrence12@users.noreply.github.com>
2021-06-23 11:23:40 +0800

    Add files via upload

---
+++ PythonLibs/fpdf/ttf-tlwg-0.7.2/Waree.php
@@ -0,0 +1,26 @@
+<?php
+$type = 'TrueType';
+$name = 'Waree';
+$desc = array('Ascent'=>1242,'Descent'=>-431,'CapHeight'=>729,'Flags'=>32,'FontBBox'=>'[-685 -431 1287 1242]','ItalicAngle'=>0,'StemV'=>70,'MissingWidth'=>600);
+$up = -78;
+$ut = 10;
+$cw = array(
+	chr(0)=>600,chr(1)=>600,chr(2)=>600,chr(3)=>600,chr(4)=>600,chr(5)=>600,chr(6)=>600,chr(7)=>600,chr(8)=>600,chr(9)=>600,chr(10)=>600,chr(11)=>600,chr(12)=>600,chr(13)=>600,chr(14)=>600,chr(15)=>600,chr(16)=>600,chr(17)=>600,chr(18)=>600,chr(19)=>600,chr(20)=>600,chr(21)=>600,
+	chr(22)=>600,chr(23)=>600,chr(24)=>600,chr(25)=>600,chr(26)=>600,chr(27)=>600,chr(28)=>600,chr(29)=>600,chr(30)=>600,chr(31)=>600,' '=>318,'!'=>401,'"'=>460,'#'=>838,'$'=>636,'%'=>950,'&'=>780,'\''=>275,'('=>390,')'=>390,'*'=>500,'+'=>838,
+	','=>318,'-'=>361,'.'=>318,'/'=>337,'0'=>636,'1'=>636,'2'=>636,'3'=>636,'4'=>636,'5'=>636,'6'=>636,'7'=>636,'8'=>636,'9'=>636,':'=>337,';'=>337,'<'=>838,'='=>838,'>'=>838,'?'=>531,'@'=>1000,'A'=>684,
+	'B'=>686,'C'=>698,'D'=>770,'E'=>632,'F'=>575,'G'=>775,'H'=>752,'I'=>295,'J'=>295,'K'=>656,'L'=>557,'M'=>863,'N'=>748,'O'=>787,'P'=>603,'Q'=>787,'R'=>695,'S'=>635,'T'=>611,'U'=>732,'V'=>684,'W'=>989,
+	'X'=>685,'Y'=>611,'Z'=>685,'['=>390,'\\'=>337,']'=>390,'^'=>838,'_'=>500,'`'=>500,'a'=>613,'b'=>635,'c'=>550,'d'=>635,'e'=>615,'f'=>352,'g'=>635,'h'=>634,'i'=>278,'j'=>278,'k'=>579,'l'=>278,'m'=>974,
+	'n'=>634,'o'=>612,'p'=>635,'q'=>635,'r'=>411,'s'=>521,'t'=>392,'u'=>634,'v'=>592,'w'=>818,'x'=>592,'y'=>592,'z'=>525,'{'=>636,'|'=>337,'}'=>636,'~'=>838,chr(127)=>600,chr(128)=>636,chr(129)=>600,chr(130)=>600,chr(131)=>600,
+	chr(132)=>600,chr(133)=>1000,chr(134)=>600,chr(135)=>600,chr(136)=>600,chr(137)=>600,chr(138)=>600,chr(139)=>600,chr(140)=>600,chr(141)=>600,chr(142)=>600,chr(143)=>600,chr(144)=>600,chr(145)=>318,chr(146)=>318,chr(147)=>518,chr(148)=>518,chr(149)=>590,chr(150)=>500,chr(151)=>1000,chr(152)=>600,chr(153)=>600,
+	chr(154)=>600,chr(155)=>600,chr(156)=>600,chr(157)=>600,chr(158)=>600,chr(159)=>600,chr(160)=>636,chr(161)=>654,chr(162)=>596,chr(163)=>640,chr(164)=>654,chr(165)=>654,chr(166)=>664,chr(167)=>513,chr(168)=>557,chr(169)=>635,chr(170)=>615,chr(171)=>669,chr(172)=>879,chr(173)=>879,chr(174)=>703,chr(175)=>703,
+	chr(176)=>571,chr(177)=>762,chr(178)=>874,chr(179)=>903,chr(180)=>649,chr(181)=>654,chr(182)=>654,chr(183)=>693,chr(184)=>586,chr(185)=>698,chr(186)=>684,chr(187)=>684,chr(188)=>659,chr(189)=>659,chr(190)=>713,chr(191)=>713,chr(192)=>703,chr(193)=>669,chr(194)=>630,chr(195)=>562,chr(196)=>654,chr(197)=>630,
+	chr(198)=>693,chr(199)=>557,chr(200)=>659,chr(201)=>693,chr(202)=>630,chr(203)=>688,chr(204)=>723,chr(205)=>596,chr(206)=>596,chr(207)=>586,chr(208)=>508,chr(209)=>0,chr(210)=>552,chr(211)=>552,chr(212)=>0,chr(213)=>0,chr(214)=>0,chr(215)=>0,chr(216)=>0,chr(217)=>0,chr(218)=>0,chr(219)=>600,
+	chr(220)=>600,chr(221)=>600,chr(222)=>600,chr(223)=>610,chr(224)=>361,chr(225)=>630,chr(226)=>454,chr(227)=>405,chr(228)=>444,chr(229)=>474,chr(230)=>586,chr(231)=>0,chr(232)=>0,chr(233)=>0,chr(234)=>0,chr(235)=>0,chr(236)=>0,chr(237)=>0,chr(238)=>0,chr(239)=>610,chr(240)=>615,chr(241)=>586,
+	chr(242)=>645,chr(243)=>684,chr(244)=>723,chr(245)=>723,chr(246)=>596,chr(247)=>752,chr(248)=>732,chr(249)=>723,chr(250)=>684,chr(251)=>1157,chr(252)=>600,chr(253)=>600,chr(254)=>600,chr(255)=>600);
+$enc = 'cp874';
+$diff = '130 /.notdef /.notdef /.notdef 134 /.notdef /.notdef /.notdef /.notdef /.notdef /.notdef /.notdef 142 /.notdef 152 /.notdef /.notdef /.notdef /.notdef /.notdef 158 /.notdef /.notdef 161 /kokaithai /khokhaithai /khokhuatthai /khokhwaithai /khokhonthai /khorakhangthai /ngonguthai /chochanthai /chochingthai /chochangthai /sosothai /chochoethai /yoyingthai /dochadathai /topatakthai /thothanthai /thonangmonthothai /thophuthaothai /nonenthai /dodekthai /totaothai /thothungthai /thothahanthai /thothongthai /nonuthai /bobaimaithai /poplathai /phophungthai /fofathai /phophanthai /fofanthai /phosamphaothai /momathai /yoyakthai /roruathai /ruthai /lolingthai /luthai /wowaenthai /sosalathai /sorusithai /sosuathai /hohipthai /lochulathai /oangthai /honokhukthai /paiyannoithai /saraathai /maihanakatthai /saraaathai /saraamthai /saraithai /saraiithai /sarauethai /saraueethai /sarauthai /sarauuthai /phinthuthai /.notdef /.notdef /.notdef /.notdef /bahtthai /saraethai /saraaethai /saraothai /saraaimaimuanthai /saraaimaimalaithai /lakkhangyaothai /maiyamokthai /maitaikhuthai /maiekthai /maithothai /maitrithai /maichattawathai /thanthakhatthai /nikhahitthai /yamakkanthai /fongmanthai /zerothai /onethai /twothai /threethai /fourthai /fivethai /sixthai /seventhai /eightthai /ninethai /angkhankhuthai /khomutthai /.notdef /.notdef /.notdef /.notdef';
+$uv = array(0=>array(0,128),128=>8364,133=>8230,145=>array(8216,2),147=>array(8220,2),149=>8226,150=>array(8211,2),160=>160,161=>array(3585,58),223=>array(3647,29));
+$file = 'Waree.z';
+$originalsize = 57564;
+$subsetted = true;
+?>
